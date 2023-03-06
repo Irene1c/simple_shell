a906617bc70a74cc69ad45b3b80696c112c33293
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-03-05 21:15:54 -0800

    simple shell

--- _exec.c
+++ _exec.c
@@ -9,21 +9,34 @@ void _execute(char **argv)
 {
 	pid_t child_pid;
 	int status;
+	char *cmd = NULL;
 
-	child_pid = fork();
-	if (child_pid == -1)
+	if (argv)
 	{
-		perror("Error:");
-	}
-	if (child_pid == 0)
-	{
-		if (execve(argv[0], argv, environ) == -1)
+		cmd = which_location(argv);
+		child_pid = fork();
+		if (child_pid == -1)
 		{
 			perror("Error:");
 		}
+		if (child_pid == 0)
+		{
+			if (execve(argv[0], argv, environ) == -1)
+			{
+				if (execve(cmd, argv, environ) == -1)
+				{
+					perror("Error:");
+				}
+				else
+				{
+					perror("Error:");
+				}
+			}
+		}
+		else
+		{
+			wait(&status);
+		}
 	}
-	else
-	{
-		wait(&status);
-	}
+	free(cmd);
 }
--- shell.h
+++ shell.h
@@ -21,5 +21,9 @@ void handler(int sig);
 void _execute(char **argv);
 int _strcmp(char *s1, char *s2);
 void _puts(char *str);
+char *which_location(char **cmd);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strdup(char *str);
 
 #endif
